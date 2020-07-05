f3047d3ebd2237055534ed2b9584e606f99e090e
Binoy K R <binoy.kr.development@gmail.com>
2020-07-05 17:23:21 +0530

    Single and grouped bar chart are added

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -5,74 +5,6 @@ function refreshDatatable(id) {
 
 };
 
-function d3LineChart(value) {
-
-    // reset svg
-
-    d3.select("div.parent").html("");
-
-    // set the dimensions and margins of the graph
-    var width = $(value.id).width() - value.margin.left - value.margin.right;
-    var height = $(value.id).height() - value.margin.top - value.margin.bottom;
-
-    // append the svg object to the body of the page
-    var svg = d3.select(value.id)
-        .append("svg")
-        .attr("width", width + value.margin.left + value.margin.right)
-        .attr("height", height + value.margin.top + value.margin.bottom)
-
-        //.attr("preserveAspectRatio", "xMinYMin meet")
-        //.attr("viewBox", "0 0 1000 400")
-
-        .append("g")
-        .attr("transform",
-            "translate(" + value.margin.left + "," + value.margin.top + ")");
-
-    // Add X axis --> it is a date format
-    var x = d3.scaleTime()
-        .domain(d3.extent(value.data, value.extent))
-        .range(value.axis.x.range(width, height));
-
-    var xaxis = svg.append("g")
-        .attr("transform", value.axis.x.transform(width, height))
-        .call(d3.axisBottom(x));
-
-    xaxis.selectAll("line")
-        .style("stroke", value.axis.stroke);
-    xaxis.selectAll("path")
-        .style("stroke", value.axis.stroke);
-    xaxis.selectAll("text")
-        .style("stroke", value.axis.text)
-        .style("font", value.axis.font);
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(value.data, value.max)])
-        .range(value.axis.y.range(width, height));
-
-    var yaxis = svg.append("g")
-        .call(d3.axisLeft(y));
-
-    yaxis.selectAll("line")
-        .style("stroke", value.axis.stroke);
-    yaxis.selectAll("path")
-        .style("stroke", value.axis.stroke);
-    yaxis.selectAll("text")
-        .style("stroke", value.axis.text)
-        .style("font", value.axis.font);
-
-
-    // Add the line
-    svg.append("path")
-        .datum(value.data)
-        .attr("fill", value.fill)
-        .attr("stroke", value.stroke)
-        .attr("stroke-width", value.strokeWidth)
-        .attr("d", d3.line()
-            .x(function (d) { return x(value.x(d)) })
-            .y(function (d) { return y(value.y(d)) })
-        )
-}
 
 
 function d3LinesChart(value) {
@@ -177,4 +109,239 @@ function d3LinesChart(value) {
         //.style("text-anchor", "end")
         .text(function (d) { return value.legend.text.value(d); });
 
+}
+
+
+
+
+function d3LineChart(value) {
+
+    // reset svg
+
+    d3.select("div.parent").html("");
+
+    // set the dimensions and margins of the graph
+    var width = $(value.id).width() - value.margin.left - value.margin.right;
+    var height = $(value.id).height() - value.margin.top - value.margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(value.id)
+        .append("svg")
+        .attr("width", width + value.margin.left + value.margin.right)
+        .attr("height", height + value.margin.top + value.margin.bottom)
+
+        //.attr("preserveAspectRatio", "xMinYMin meet")
+        //.attr("viewBox", "0 0 1000 400")
+
+        .append("g")
+        .attr("transform",
+            "translate(" + value.margin.left + "," + value.margin.top + ")");
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+        .domain(d3.extent(value.data, value.extent))
+        .range(value.axis.x.range(width, height));
+
+    var xaxis = svg.append("g")
+        .attr("transform", value.axis.x.transform(width, height))
+        .call(d3.axisBottom(x));
+
+    xaxis.selectAll("line")
+        .style("stroke", value.axis.stroke);
+    xaxis.selectAll("path")
+        .style("stroke", value.axis.stroke);
+    xaxis.selectAll("text")
+        .style("stroke", value.axis.text)
+        .style("font", value.axis.font);
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(value.data, value.max)])
+        .range(value.axis.y.range(width, height));
+
+    var yaxis = svg.append("g")
+        .call(d3.axisLeft(y));
+
+    yaxis.selectAll("line")
+        .style("stroke", value.axis.stroke);
+    yaxis.selectAll("path")
+        .style("stroke", value.axis.stroke);
+    yaxis.selectAll("text")
+        .style("stroke", value.axis.text)
+        .style("font", value.axis.font);
+
+
+    // Add the line
+    svg.append("path")
+        .datum(value.data)
+        .attr("fill", value.fill)
+        .attr("stroke", value.stroke)
+        .attr("stroke-width", value.strokeWidth)
+        .attr("d", d3.line()
+            .x(function (d) { return x(value.x(d)) })
+            .y(function (d) { return y(value.y(d)) })
+        )
+}
+
+
+function d3BarChart(value) {
+
+    // reset svg
+    d3.select("div.parent").html("");
+
+    // set the dimensions and margins of the graph
+    var width = $(value.id).width() - value.margin.left - value.margin.right;
+    var height = $(value.id).height() - value.margin.top - value.margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(value.id)
+        .append("svg")
+        .attr("width", width + value.margin.left + value.margin.right)
+        .attr("height", height + value.margin.top + value.margin.bottom);
+
+    var g = svg.append("g")
+        .attr("transform",
+            "translate(" + value.margin.left + "," + value.margin.top + ")");
+
+    var xScale = d3.scaleBand().range(value.axis.x.range(width, height)).padding(0.4),
+        yScale = d3.scaleLinear().range(value.axis.y.range(width, height));
+
+    xScale.domain(value.data.map(function (d) { return value.extent(d); }));
+    yScale.domain([0, d3.max(value.data, function (d) { return value.max(d); })]);
+
+    var xaxis = g.append("g")
+        .attr("transform", value.axis.x.transform(width, height))
+        .call(d3.axisBottom(xScale));
+
+    xaxis.selectAll("line")
+        .style("stroke", value.axis.stroke);
+    xaxis.selectAll("path")
+        .style("stroke", value.axis.stroke);
+    xaxis.selectAll("text")
+        .style("stroke", value.axis.text)
+        .style("font", value.axis.font);
+
+    var yaxis = g.append("g")
+        .call(d3.axisLeft(yScale)
+
+            // .tickFormat(function (d) {
+            //     return "$" + d;
+            // }).ticks(10)
+
+        );
+
+
+    yaxis.selectAll("line")
+        .style("stroke", value.axis.stroke);
+    yaxis.selectAll("path")
+        .style("stroke", value.axis.stroke);
+    yaxis.selectAll("text")
+        .style("stroke", value.axis.text)
+        .style("font", value.axis.font);
+
+
+    g.selectAll(".bar")
+        .data(value.data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return xScale(value.x(d)); })
+        .attr("y", function (d) { return yScale(value.y(d)); })
+        .attr("fill", value.fill)
+        .attr("stroke", value.stroke)
+        .attr("stroke-width", value.strokeWidth)
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) { return height - yScale(d.value); });
+}
+
+function d3BarChartGroup(value) {
+
+    // reset svg
+    d3.select("div.parent").html("");
+
+    // set the dimensions and margins of the graph
+    var width = $(value.id).width() - value.margin.left - value.margin.right;
+    var height = $(value.id).height() - value.margin.top - value.margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(value.id)
+        .append("svg")
+        .attr("width", width + value.margin.left + value.margin.right)
+        .attr("height", height + value.margin.top + value.margin.bottom);
+
+    var g = svg.append("g")
+        .attr("transform",
+            "translate(" + value.margin.left + "," + value.margin.top + ")");
+
+    var xScale0 = d3.scaleBand().range([0, width]).padding(.2)
+    var xScale1 = d3.scaleBand()
+    var yScale = d3.scaleLinear().range([height, 0])
+
+    var xAxis = d3.axisBottom(xScale0);
+    var yAxis = d3.axisLeft(yScale);
+
+    xScale0.domain(value.data.map(d => d.model_name))
+    xScale1.domain(['field1', 'field2']).range([0, xScale0.bandwidth()])
+    yScale.domain([0, d3.max(value.data, d => d.field1 > d.field2 ? d.field1 : d.field2)])
+
+
+    var model_name = svg.selectAll(".model_name")
+        .data(value.data)
+        .enter().append("g")
+        .attr("class", "model_name")
+        .attr("transform", d => 'translate(' + (xScale0(d.model_name) + value.margin.left) + ', ' + value.margin.top + ')');
+
+
+    /* Add field1 bars */
+    model_name.selectAll(".bar.field1")
+        .data(d => [d])
+        .enter()
+        .append("rect")
+        .attr("class", "bar field1")
+        .style("fill", "blue")
+        .attr("x", d => xScale1('field1'))
+        .attr("y", d => yScale(d.field1))
+        .attr("width", xScale1.bandwidth())
+        .attr("height", d => {
+            return height - yScale(d.field1)
+        });
+
+    /* Add field2 bars */
+    model_name.selectAll(".bar.field2")
+        .data(d => [d])
+        .enter()
+        .append("rect")
+        .attr("class", "bar field2")
+        .style("fill", "red")
+        .attr("x", d => xScale1('field2'))
+        .attr("y", d => yScale(d.field2))
+        .attr("width", xScale1.bandwidth())
+        .attr("height", d => {
+            return height - yScale(d.field2)
+        });
+
+    // Add the X Axis
+    var xaxis = g.append("g")
+        .attr("transform", value.axis.x.transform(width, height))
+        .call(xAxis);
+
+    xaxis.selectAll("line")
+        .style("stroke", value.axis.stroke);
+    xaxis.selectAll("path")
+        .style("stroke", value.axis.stroke);
+    xaxis.selectAll("text")
+        .style("stroke", value.axis.text)
+        .style("font", value.axis.font);
+
+
+    // Add the Y Axis
+    var yaxis = g.append("g")
+        .call(yAxis);
+
+    yaxis.selectAll("line")
+        .style("stroke", value.axis.stroke);
+    yaxis.selectAll("path")
+        .style("stroke", value.axis.stroke);
+    yaxis.selectAll("text")
+        .style("stroke", value.axis.text)
+        .style("font", value.axis.font);
 }
--- src/pages/D3ChartPage.js
+++ src/pages/D3ChartPage.js
@@ -24,24 +24,29 @@ class D3ChartPage extends React.Component {
         this.sampleGroupedData = this.sampleGroupedData.bind(this);
         this.populateGrouped = this.populateGrouped.bind(this);
         this.colours = this.colours.bind(this);
+        this.populateBarChart = this.populateBarChart.bind(this);
+        this.sampleSingleBarData = this.sampleSingleBarData.bind(this);
+        this.populateBarChartGrouped = this.populateBarChartGrouped.bind(this);
+        this.sampleBarGroupedData = this.sampleBarGroupedData.bind(this);
 
     }
 
-
     componentDidMount() {
         let self = this;
-
 
         var data1 = this.sampleGroupedData();
         self.populateGrouped(data1);
 
         var data2 = this.sampleSingleData();
         self.populateAll(data2);
+
+        var data3 = this.sampleSingleBarData();
+        self.populateBarChart(data3);
+
+        var data4 = this.sampleBarGroupedData();
+        self.populateBarChartGrouped(data4);
     }
 
-    formatDate(date) {
-        return window.d3.timeParse("%Y-%m-%d %H:%M:%S")(date);
-    }
 
     populateAll(data) {
 
@@ -54,15 +59,15 @@ class D3ChartPage extends React.Component {
             data: data,
             axis: {
                 x: {
-                    transform: function(width, height) {
+                    transform: function (width, height) {
                         return "translate(0," + height + ")";
                     },
-                    range: function(width, height) {
+                    range: function (width, height) {
                         return [0, width];
                     }
                 },
                 y: {
-                    range: function(width, height) {
+                    range: function (width, height) {
                         return [height, 0];
                     }
                 },
@@ -88,15 +93,15 @@ class D3ChartPage extends React.Component {
             items: data.items,
             axis: {
                 x: {
-                    transform: function(width, height) {
+                    transform: function (width, height) {
                         return "translate(0," + height + ")";
                     },
-                    range: function(width, height) {
-                        return [0, width - 200];
+                    range: function (width, height) {
+                        return [0, width - 100];
                     }
                 },
                 y: {
-                    range: function(width, height) {
+                    range: function (width, height) {
                         return [height, 0];
                     }
                 },
@@ -106,14 +111,14 @@ class D3ChartPage extends React.Component {
                 font: "10px Arial"
             },
             legend: {
-                transform: function(d, i, width, height){
+                transform: function (d, i, width, height) {
                     return 'translate(' + 0 + ',' + (i * 20) + ')';
                 },
                 rect: {
                     width: 10,
                     height: 10,
                     x: function (width, height) {
-                        return width - 180;
+                        return width - 100;
                     },
                     y: function (width, height) {
                         return 0;
@@ -121,7 +126,7 @@ class D3ChartPage extends React.Component {
                 },
                 text: {
                     x: function (width, height) {
-                        return width - 160;
+                        return width - 80;
                     },
                     y: function (width, height) {
                         return 10;
@@ -141,9 +146,118 @@ class D3ChartPage extends React.Component {
 
     }
 
-    colours() {
-        return ["ORCHID", "GOLD", "ORANGERED", "LIMEGREEN", "HOTPINK", "MEDIUMVIOLETRED", "DARKVIOLET"];
+
+    populateBarChart(data) {
+
+        window.d3BarChart({
+            id: "#chart_3",
+            margin: { top: 10, right: 30, bottom: 30, left: 60 },
+            fill: 'steelblue',
+            stroke: "steelblue",
+            strokeWidth: "1px",
+            data: data,
+            axis: {
+                x: {
+                    transform: function (width, height) {
+                        return "translate(0," + height + ")";
+                    },
+                    range: function (width, height) {
+                        return [0, width];
+                    }
+                },
+                y: {
+                    range: function (width, height) {
+                        return [height, 0];
+                    }
+                },
+                stroke: "#818896",
+                fill: "none",
+                text: "#818896",
+                font: "10px Arial"
+            },
+            extent: function (d) { return d.date; },
+            max: function (d) { return d.value; },
+            x: function (d) { return d.date },
+            y: function (d) { return d.value }
+        });
     }
+
+    populateBarChartGrouped(data) {
+
+        window.d3BarChartGroup({
+            id: "#chart_4",
+            margin: { top: 10, right: 30, bottom: 30, left: 60 },
+            fill: 'steelblue',
+            stroke: "steelblue",
+            strokeWidth: "1px",
+            data: data,
+            axis: {
+                x: {
+                    transform: function (width, height) {
+                        return "translate(" + 0 + "," + height + ")";
+                    },
+                    range: function (width, height) {
+                        return [0, width];
+                    }
+                },
+                y: {
+                    range: function (width, height) {
+                        return [height, 0];
+                    }
+                },
+                stroke: "#818896",
+                fill: "none",
+                text: "#818896",
+                font: "10px Arial"
+            },
+            extent: function (d) { return d.date; },
+            max: function (d) { return d.value; },
+            x: function (d) { return d.date },
+            y: function (d) { return d.value }
+        });
+    }
+
+    render() {
+
+        return (
+            <PageContent>
+                <Row>
+                    <PagePanel cols="col-xxl-6 col-lg-6" >
+                        <PagePanelHead title="Single Line">
+                        </PagePanelHead>
+                        <PagePanelBody>
+                            <div id="chart_1" style={{ height: "300px" }}></div>
+                        </PagePanelBody>
+                    </PagePanel>
+                    <PagePanel cols="col-xxl-6 col-lg-6" >
+                        <PagePanelHead title="Multipe Line & Legend">
+                        </PagePanelHead>
+                        <PagePanelBody>
+                            <div id="chart_2" style={{ height: "300px" }}></div>
+                        </PagePanelBody>
+                    </PagePanel>
+                </Row>
+                <Row>
+                    <PagePanel cols="col-xxl-6 col-lg-6" >
+                        <PagePanelHead title="Bar Chart">
+                        </PagePanelHead>
+                        <PagePanelBody>
+                            <div id="chart_3" style={{ height: "300px" }}></div>
+                        </PagePanelBody>
+                    </PagePanel>
+                    <PagePanel cols="col-xxl-6 col-lg-6" >
+                        <PagePanelHead title="Bar Chart Grouped">
+                        </PagePanelHead>
+                        <PagePanelBody>
+                            <div id="chart_4" style={{ height: "300px" }}></div>
+                        </PagePanelBody>
+                    </PagePanel>
+                </Row>
+            </PageContent>
+        );
+
+    }
+
 
     sampleSingleData() {
         let self = this;
@@ -217,34 +331,48 @@ class D3ChartPage extends React.Component {
         return { values: values, items: items, legends: legends };
     }
 
-    render() {
-
-        return (
-            <PageContent>
-                <Row>
-                    <PagePanel cols="col-xxl-7 col-lg-12" >
-                        <PagePanelHead title="Single Line">
-                        </PagePanelHead>
-                        <PagePanelBody>
-                            <div id="chart_1" style={{ height: "300px" }}></div>
-                        </PagePanelBody>
-                    </PagePanel>
-                </Row>
-                <Row>
-                    <PagePanel cols="col-xxl-7 col-lg-12" >
-                        <PagePanelHead title="Multipe Line">
-                        </PagePanelHead>
-                        <PagePanelBody>
-                            <div id="chart_2" style={{ height: "300px" }}></div>
-                        </PagePanelBody>
-                    </PagePanel>
-                </Row>
-            </PageContent>
-        );
-
+    formatDate(date) {
+        return window.d3.timeParse("%Y-%m-%d %H:%M:%S")(date);
     }
 
+    colours() {
+        return ["ORCHID", "GOLD", "ORANGERED", "LIMEGREEN", "HOTPINK", "MEDIUMVIOLETRED", "DARKVIOLET"];
+    }
 
+    sampleSingleBarData() {
+        let self = this;
+
+        return [
+            { date: 2011, value: 45 },
+            { date: 2012, value: 47 },
+            { date: 2013, value: 52 },
+            { date: 2014, value: 70 },
+            { date: 2015, value: 75 },
+            { date: 2016, value: 78 },
+        ];
+    }
+
+    sampleBarGroupedData() {
+        let self = this;
+
+        return [
+            {
+                "model_name": "f1",
+                "field1": 19,
+                "field2": 83
+            },
+            {
+                "model_name": "f2",
+                "field1": 67,
+                "field2": 93
+            },
+            {
+                "model_name": "f3",
+                "field1": 10,
+                "field2": 56
+            },
+        ];
+    }
 }
 
 export default D3ChartPage;
